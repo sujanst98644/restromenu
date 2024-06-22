0bf15d5248bfcffd7e47947571ecebf986e632bd
Sujan Thapa <sujan.st98644@gmail.com>
2024-06-22 11:43:06 +0545

    functions and selection added

--- src/App.js
+++ src/App.js
@@ -3,25 +3,18 @@ import data from "./data"
 
 function App() {
   const [isHover, setHover] = useState(false)
-  // const [Scrolled, setScrolled] = useState(false)
+  const [singleSelection, setSingleSelection] =useState(null)
 
-  // useEffect(()=>{
-  //   const handleScroll=()=>{
-  //     const isScrolled=window.scrollY>40
-  //     if(isScrolled!==Scrolled){
-  //       setScrolled(isScrolled)
-  //     }
-  //   }
-  //   window.addEventListener('scroll',handleScroll)
-  //   return()=>{
-  //     window.removeEventListener('scroll',handleScroll)
-  //   }
-  // },[Scrolled])
+  const handleSelect= (getCurrentId) =>{
+    console.log(getCurrentId);
+    setSingleSelection(getCurrentId === singleSelection ? null : getCurrentId)
+  }
+  console.log(singleSelection);
   return (
     <main className="flex flex-col font-sora text-white bg-[#EEF7FF]">
         <header className={`grid lg:grid-cols-3 items-center  pl-8 top-0 left-0 w-full`}>
           <div className="flex flex-row items-center">
-            <img src="Burger.png" alt="logo" className="h-14" />
+            <img src={`${process.env.PUBLIC_URL}/Burger.png`} alt="logo" className="h-14" />
             <p className="font-bold hover:scale-110 text-black">crunchy</p>
           </div>
           <div className="font-sora col-start-3 justify-end">
@@ -34,49 +27,31 @@ function App() {
         <div className="h-96 w-auto bg-[#CDE8E5] text-green-700 font-sora font-semibold text-6xl text-center pt-40">
             <div className="items-center">Discover Our Menu</div>
         </div>
-      <div className="h-full grid grid-cols-4 gap-4 mx-5 mt-16 text-green-700 items-center justify-center">
+      <div className="h-full grid gap-4 mx-5 mt-16 lg:grid-cols-4 text-green-700 items-center justify-center">
           {data.map(item=>{return(
-          <div className={` shadow-xl rounded-xl h-[500px] w-80 grid grid-row-7 items-center justify-center ${isHover ? 'transition-colors hover:bg-[#7AB2B2] duration-500 ease-in-out' : 'bg-[#EEF7FF]'}`}
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}>
+          <div className={` shadow-xl rounded-xl lg:h-[500px] lg:w-80 h-[200px] w-96 lg:grid lg:grid-row-7 flex flex-col items-center
+            ${singleSelection === item.id ? 'transition-colors hover:bg-[#7AB2B2] duration-500 ease-in-out' : 'bg-[#EEF7FF]'}`}
+          onMouseEnter={() => {setHover(true)
+            handleSelect(item.id)
+          } }
+            onMouseLeave={() => {setHover(false) 
+              setSingleSelection(null)
+            }}>
               <p className="text-black self-start ml-4 mt-4 font-bold">{item.price}</p>
-              <div className="flex items-center justify-center row-span-4">
-              <img src= {item.img}  className="w-64 h-64 rounded-full object-cover shadow-xl"/>
+              <div className="lg:grid flex flex-row items-center justify-center">
+              <div className="flex items-center lg:justify-center justify-start lg:row-span-4">
+              <img src= {item.img}  className="lg:w-64 lg:h-64 w-32 h-32 rounded-full object-cover shadow-xl mx-4"/>
               </div>
-              <div className="text-center">
+              <div className="text-center h-[150px] w-[210px] lg:w-[300px]">
                 <p className="text-xl font-bold m-3">{item.cuisine}</p>
-              <p className={`tracking-tighter mx-4 mb-4 text-sm `}>{item.ingredients}</p>
-              {/* <button className={`text-center`}>Order Online</button> */}
+                {singleSelection === item.id ? "":<p className={`tracking-tighter mx-4 mb-4 text-sm`}>{item.ingredients}</p>}
+                {singleSelection === item.id ? <button className={`text-center transition duration-500 ease-in-out  border-2 bg-green-800 text-white rounded-xl px-4 py-2`}>Order Online</button> : "" }
+              </div>
               </div>
             </div>
           )}
           )
           }
-          
-          {/* <div className=" bg-slate-700 shadow-xl rounded-xl h-[500px] w-80 flex flex-col items-center justify-center">
-            <img src="Momo.jpg" className="w-64 h-64 rounded-full object-cover"/>
-          <p className="text-3xl m-3">Momo</p>
-          </div>
-          <div className="shadow-xl rounded-xl h-[500px] w-80 flex flex-col items-center justify-center">
-              <img src="Thukpa.jpg" className="w-64 h-64 rounded-full object-cover"/>
-          <p className=" text-3xl m-3">Thupka</p>
-          </div>
-          <div className="shadow-xl rounded-xl h-[500px] w-80 flex flex-col items-center justify-center">
-              <img src="Sekuwa.jpg" className="w-64 h-64 rounded-full object-cover"/>
-          <p className=" text-3xl m-3">Sekuwa</p>
-          </div>
-          <div className="shadow-xl rounded-xl h-[500px] w-80 flex flex-col items-center justify-center">
-              <img src="Thakali.jpg" className="w-64 h-64 rounded-full object-cover"/>
-          <p className=" text-3xl m-3">Thakali khana</p>
-          </div>
-          <div className="shadow-xl h-[500px] w-80 rounded-xl flex flex-col items-center justify-center">
-              <img src="Samai.jpg" className="w-64 h-64 rounded-full object-cover"/>
-          <p className="text-3xl m-3"></p>
-          </div>
-          <div className="shadow-xl rounded-xl col-span-2"
-            style={{ backgroundImage: "url('')" }} >
-          <p className="text-3xl m-3">Momo</p>
-          </div> */}
       </div>
     </main>
   );
--- src/data.js
+++ src/data.js
@@ -1,40 +1,47 @@
 export default [
     {
-        img: "momo.jpg",
+        img: `${process.env.PUBLIC_URL}/momo.jpg`,
         cuisine: "Momo",
         price:"$1.5",
-        ingredients:"chopped vegetables or ground meat, chilies, and cilantro."
-    },
-    {
-        img: "Thukpa.jpg",
-        cuisine: "Thukpa",
-        price:"$1.4",
-        ingredients:"noodle soup made with meat or vegetables, noodles, and spices."
+        ingredients:"chopped vegetables or ground meat, chilies, and cilantro.",
+        id: 1
 
     },
     {
-        img: "samai.jpg",
+        img: `${process.env.PUBLIC_URL}/Thukpa.jpg`,
+        cuisine: "Thukpa",
+        price:"$1.4",
+        ingredients:"noodle soup made with meat or vegetables, noodles, and spices.",
+        id: 2
+
+    },
+    {
+        img: `${process.env.PUBLIC_URL}/samai.jpg`,
         cuisine: "Samai",
         price:"$7.25",
-        ingredients:"buckwheat, millet, or maize flour, gundruk, spiced meat."
+        ingredients:"buckwheat, millet, or maize flour, gundruk, spiced meat.",
+        id: 3
     },
     {
-        img: "Thakali.jpg",
+        img: `${process.env.PUBLIC_URL}/Thakali.jpg`,
         cuisine: "Thakali Khana",
         price:"$7",
-        ingredients:"rice, lentil soup, gundruk, spiced meat, achar, and vegetables."
+        ingredients:"rice, lentil soup, gundruk, spiced meat, achar, and vegetables.",
+        id: 4
     },
     {
-        img: "Sekuwa.jpg",
+        img: `${process.env.PUBLIC_URL}/Sekuwa.jpg`,
         cuisine: "Sekuwa",
         price:"$5",
-        ingredients:"skewered grilled meat with cumin, coriander, turmeric, and chili."
+        ingredients:"skewered grilled meat with cumin, coriander, turmeric, and chili.",
+        id: 5
     },
     {
-        img: "dhedo-Food.jpeg",
+        img: `${process.env.PUBLIC_URL}/dhedo-Food.jpeg`,
         cuisine: "Dhedo",
         price:"$15",
-        ingredients:"buckwheat, millet, or maize flour, served with gundruk , achar"
+        ingredients:"buckwheat, millet, or maize flour, served with gundruk , achar",
+        id: 6
     },
 
 ]
